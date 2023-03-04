b013675815777d3668c687772c1e63c75a69a503
Mahmoud Elshafaey <mahmoud.elshafaey@linkdev.com>
2023-03-04 18:52:05 +0200

    replicas

--- set_env.sh
+++ set_env.sh
@@ -8,4 +8,4 @@ export AWS_BUCKET=my-183386466629-bucket
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
-export URL=http://localhost:8100
+export URL=http://localhost:8200
